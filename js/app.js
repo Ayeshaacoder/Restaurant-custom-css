1851e311f1c332a5ada0e4dd42f2aad76a1d90ce
“[Ayesha <ayeshakhan02753@gmail.com>
2023-02-21 01:35:49 +0530

    Created a Food website

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   $('.food-slider').slick({
+      autoplay:true,
+      slidesToShow:3,
+      slidesTOScroll:1,
+      prevArrow:".prev-btn",
+      nextArrow:".next-btn",
+   })
+
+});
